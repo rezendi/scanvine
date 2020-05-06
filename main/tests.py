06bc336d5b69f09edadc68c7660b322eb7355126
Jon Evans <jon@rezendi.com>
2020-05-06 15:54:41 -0700

    kind of a real test now

--- main/tests.py
+++ main/tests.py
@@ -9,3 +9,14 @@ class MetadataParserTests(TestCase):
         self.assertEqual("TestTitle1", metadata['sv_title'])
         self.assertEqual("TestAuthor1", metadata['sv_author'])
 
+    def test_share_fetch(self):
+        html = '<html><head><title>TestTitle1</title><meta name="author" content="TestAuthor1"></html>'
+        tasks.add_tweet(1258151068892094468)
+        shares = models.Share.objects.filter(twitter_id=1258151068892094468)
+        self.assertTrue(len(shares)>0)
+        share = shares[0]
+        self.assertIsNotNone(share.article_id)
+        article = models.Article.objects.get(id=share.article_id)
+        tasks.parse_article_metadata(article.id)
+        article.refresh_from_db()
+        self.assertIsNotNone(article.author_id)
