0a56cc95df73ea48dad3bcb4ff715cf9e93b477d
Jon Evans <jon@rezendi.com>
2020-05-12 10:04:55 -0700

    add an exclusion

--- main/article_parsers.py
+++ main/article_parsers.py
@@ -262,7 +262,7 @@ def clean_author_name(name, publication_name):
     exclusions = [publication_name] if publication_name else []
     exclusions+= ["associated press", "health correspondent", "opinion columnist", "opinion contributor", "commissioning editor", "special correspondent"]
     exclusions+= ["correspondent", "contributor", "columnist", "editor," "editor-at-large"]
-    exclusions+= ["business", "news", "with", "by", "about", " the", "author", "posted", "on", "👤by"]
+    exclusions+= ["business", "news", "with", "by", "about", " the", "author", "posted", "on", "👤by", "and"]
     exclusions+= ["reuters", "AP", "AFP", "|"]
     newname = name if name else ''
     newname = re.sub(r'<[^>]*>', "", newname)
