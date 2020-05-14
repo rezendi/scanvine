c867090bed275a67ac43374d1ecfed11c8f1df74
Jon Evans <jon@rezendi.com>
2020-05-13 17:16:07 -0700

    basic sorting

--- main/views.py
+++ main/views.py
@@ -2,6 +2,15 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import *
 
+SORT_BY = {
+    'dc':'-created_at',
+    'oc':'-created_at',
+    'ds':'-total_credibility',
+    'os':'total_credibility',
+    'da':'-average_credibility',
+    'a':'average_credibility',
+}
+
 def index(request):
     template = loader.get_template('main/index.html')
     page_size = int(request.GET.get('s', '10'))
@@ -36,7 +45,8 @@ def publication(request, publication_id):
 def authors(request):
     template = loader.get_template('main/authors.html')
     page_size = int(request.GET.get('s', '10'))
-    authors = Author.objects.all().order_by('-total_credibility')[:page_size]
+    sort = request.GET.get('o', 'ds')
+    authors = Author.objects.all().order_by(SORT_BY[sort])[:page_size]
     for author in authors:
         author.total_articles = Article.objects.filter(author_id=author.id).count()
         latest = Article.objects.filter(author_id=author.id).order_by("-created_at")[:1]
@@ -49,10 +59,11 @@ def authors(request):
 def publications(request):
     template = loader.get_template('main/publications.html')
     page_size = int(request.GET.get('s', '10'))
-    publications = Publication.objects.all().order_by('-total_credibility')[0:page_size]
+    sort = request.GET.get('o', 'ds')
+    publications = Publication.objects.all().order_by(SORT_BY[sort])[:page_size]
     for publication in publications:
         publication.total_articles = Article.objects.filter(publication_id=publication.id).count()
-        latest = Article.objects.filter(publication_id=publication.id).order_by("-created_at")[:1]
+        latest = Article.objects.filter(publication_id=publication.id).order_by('-created_at')[:1]
         publication.latest = latest[0] if latest else None
     context = {
         'publications': publications,
