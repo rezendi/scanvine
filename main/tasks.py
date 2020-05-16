2ad1739a50bf96e391ee66b92ba8b56bc9da48fe
Jon Evans <jon@rezendi.com>
2020-05-16 01:12:53 -0700

    author cleanup

--- main/tasks.py
+++ main/tasks.py
@@ -466,11 +466,15 @@ def set_scores(date=datetime.datetime.utcnow().date(), days=7):
             else:
                 article.scores['publisher_average'] = 0 
             article.save()
-            author_ids = [article.author.id]
-            collaborators = Collaboration.objects.filter(partnership_id=article.author.id)
+
+            # allocate author scores
+            author_ids = [article.author_id]
+            author_amount = amount
+            collaborators = Collaboration.objects.filter(partnership_id=article.author_id)
             if collaborators:
                 author_ids = [c.individual_id for c in collaborators]
-            author_amount = amount / len(author_ids)
+                author_ids = [article.author_id] if not author_ids else author_ids
+                author_amount = amount / len(author_ids)
             for author_id in author_ids:
                 if not author_id in authors_dict:
                     authors_dict[author_id] = 0
