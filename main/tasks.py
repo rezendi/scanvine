a08de93a9fc0903d8db7c47f1d4a083961b519c4
Jon Evans <jon@rezendi.com>
2020-05-20 16:58:08 -0700

    self sharing fix

--- main/tasks.py
+++ main/tasks.py
@@ -380,7 +380,7 @@ def parse_article_metadata(article_id):
         if author and len(author.name) > 6:
             for share in Share.objects.filter(article_id=article.id):
                 if share.sharer_id and share.sharer.name == author.name:
-                    Tranche.filter(receiver=share.id).delete()
+                    Tranche.objects.filter(receiver=share.id).delete()
                     share.status = Share.Status.SELF_SHARE
                     share.save()
 
