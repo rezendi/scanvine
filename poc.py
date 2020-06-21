c54f2b6c93939389ca47a17a089e62a3df2329fc
Jon Evans <jon@rezendi.com>
2020-06-21 13:59:36 -0700

    POC thread capture

--- poc.py
+++ poc.py
@@ -46,5 +46,21 @@ if not settings.configured:
     django.setup()
     celery = Celery('scanvine', backend='rpc://')
 
+import dateparser
 from main.metatasks import *
-auto_tweet()
+
+POC_TWEET_ID = '1274579726473007106'
+MIN_RETWEETS = 10
+
+t = api.GetStatus(POC_TWEET_ID, include_entities=True)
+if t.retweet_count > MIN_RETWEETS:
+    handle = t.user.screen_name
+    term = "from:%s to:%s" % (handle, handle)
+    since = dateparser.parse(t.created_at.rpartition(" ")[0])
+    since_str = since.strftime('%Y-%m-%d')
+    until = since + datetime.timedelta(days=1)
+    until_str = until.strftime('%Y-%m-%d')
+    sr = api.GetSearch(term=term, since=since_str, until=until_str, since_id=t.id, count=100, result_type="recent", lang='en', include_entities = True)
+    for r in sr:
+        print("irs %s r %s" % (r.in_reply_to_status_id, r.full_text))
+    
