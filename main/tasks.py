135059f842323e5e673510bc1609a484950e9b39
Jon Evans <jon@rezendi.com>
2020-06-22 18:36:21 -0700

    attach shares to existing thread articles

--- main/tasks.py
+++ main/tasks.py
@@ -289,22 +289,28 @@ def get_twitter_thread(tweet_id, sharer_id, root_tweet_id, root_tweet_text, root
         }
         root_thread_url = "https://twitter.com/%s/status/%s" % (thread[0].user.screen_name, thread[0].id)
         
-        article = Article(status=Article.Status.AUTHOR_NOT_FOUND, language='en', title = title, metadata=metadata, contents='',
-                          initial_url = root_tweet_url, url = root_thread_url, author_id = None, publication = publication)
-        article.save()
-        # find and associate author
-        existing_auth = Author.objects.filter(twitter_screen_name__iexact=screen_name)
-        author = existing_auth[0] if existing_auth else article_parsers.get_author_for(metadata, article.publication)
-        if author:
-            article.author_id = author.id
-            article.status = Article.Status.AUTHOR_ASSOCIATED
-            log_job(job, "Author %s associated to article %s" % (author.name, article.url))
+        article = None
+        existing = Article.objects.filter(url = url).order_by("created_at")
+        if existing:
+            article = existing[0]
+        else:
+            article = Article(status=Article.Status.AUTHOR_NOT_FOUND, language='en', title = title, metadata=metadata, contents='',
+                              initial_url = root_tweet_url, url = root_thread_url, author_id = None, publication = publication)
             article.save()
-        log_job(job, "Thread article created %s" % article.id, Job.Status.COMPLETED)
+
+            # find and associate author
+            existing_auth = Author.objects.filter(twitter_screen_name__iexact=screen_name)
+            author = existing_auth[0] if existing_auth else article_parsers.get_author_for(metadata, article.publication)
+            if author:
+                article.author_id = author.id
+                article.status = Article.Status.AUTHOR_ASSOCIATED
+                log_job(job, "Author %s associated to article %s" % (author.name, article.url))
+                article.save()
 
         share = Share(source=0, language='en', status=Share.Status.ARTICLE_ASSOCIATED, category=sharer.category,
                       sharer_id=sharer.id, twitter_id=root_tweet_id, text=root_tweet_text, url=root_tweet_url, article_id = article.id)
         share.save()
+        log_job(job, "Thread article created %s" % article.id, Job.Status.COMPLETED)
     
     except Exception as ex:
         log_job(job, traceback.format_exc())
