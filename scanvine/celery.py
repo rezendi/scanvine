e5f789f581284e90db776792856098fd1474f89f
Jon Evans <jon@rezendi.com>
2020-11-24 12:14:14 -0800

    sunset for now

--- scanvine/celery.py
+++ scanvine/celery.py
@@ -45,52 +45,55 @@ app.conf.task_routes = {
 
 if 'SCANVINE_ENV' in os.environ and os.environ['SCANVINE_ENV']=='production':
     app.conf.task_always_eager = False
-    app.conf.beat_schedule = {
-        'add-every-30-seconds': {
-            'task': 'main.tasks.fetch_shares',
-            'schedule': 60.0,
-        },
-        'add-every-180-seconds': {
-            'task': 'main.tasks.analyze_sentiment',
-            'schedule': 183.0,
-        },
-        'add-every-300-seconds': {
-            'task': 'main.tasks.associate_articles',
-            'schedule': 303.0,
-        },
-        'add-every-450-seconds': {
-            'task': 'main.tasks.reparse_articles',
-            'schedule': 459.0,
-        },
-        'add-every-1200-seconds': {
-            'task': 'main.tasks.allocate_credibility',
-            'schedule': 1212.0,
-        },
-        'add-every-1200-seconds-2': {
-            'task': 'main.metatasks.auto_tweet',
-            'schedule': 1223.0,
-        },
-        'add-every-2400-seconds': {
-            'task': 'main.tasks.refresh_sharers',
-            'schedule': 2448.0,
-        },
-        'add-every-9000-seconds': {
-            'task': 'main.metatasks.clean_up',
-            'schedule': 9009.0,
-        },
-        'add-every-9000-seconds-2': {
-            'task': 'main.tasks.parse_unparsed_articles',
-            'schedule': 9199.0,
-        },
-        'add-every-10800-seconds': {
-            'task': 'main.tasks.ingest_sharers',
-            'schedule': 10801.0,
-        },
-        'add-every-12000-seconds-3': {
-            'task': 'main.tasks.regurgitate_sharers',
-            'schedule': 12199.0,
-        },
-    }
+    if False:
+        app.conf.beat_schedule = {
+            'add-every-30-seconds': {
+                'task': 'main.tasks.fetch_shares',
+                'schedule': 60.0,
+            },
+            'add-every-180-seconds': {
+                'task': 'main.tasks.analyze_sentiment',
+                'schedule': 183.0,
+            },
+            'add-every-300-seconds': {
+                'task': 'main.tasks.associate_articles',
+                'schedule': 303.0,
+            },
+            'add-every-450-seconds': {
+                'task': 'main.tasks.reparse_articles',
+                'schedule': 459.0,
+            },
+            'add-every-1200-seconds': {
+                'task': 'main.tasks.allocate_credibility',
+                'schedule': 1212.0,
+            },
+            'add-every-1200-seconds-2': {
+                'task': 'main.metatasks.auto_tweet',
+                'schedule': 1223.0,
+            },
+            'add-every-2400-seconds': {
+                'task': 'main.tasks.refresh_sharers',
+                'schedule': 2448.0,
+            },
+            'add-every-9000-seconds': {
+                'task': 'main.metatasks.clean_up',
+                'schedule': 9009.0,
+            },
+            'add-every-9000-seconds-2': {
+                'task': 'main.tasks.parse_unparsed_articles',
+                'schedule': 9199.0,
+            },
+            'add-every-10800-seconds': {
+                'task': 'main.tasks.ingest_sharers',
+                'schedule': 10801.0,
+            },
+            'add-every-12000-seconds-3': {
+                'task': 'main.tasks.regurgitate_sharers',
+                'schedule': 12199.0,
+            },
+        }
+    else:
+        app.conf.beat_schedule = {}
 else:
     app.conf.task_always_eager = True
 
